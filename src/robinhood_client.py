be73195906e23567e626d5c1a8cd9d9c226119d7
adityak74 <akarnam37@gmail.com>
2022-06-04 02:22:34 -0500

    feat: auto mfa login

--- src/robinhood_client.py
+++ src/robinhood_client.py
@@ -1,5 +1,5 @@
 import os
-
+import pyotp
 import robin_stocks.robinhood as r
 from dotenv import load_dotenv
 
@@ -13,8 +13,14 @@ class RobinhoodClient:
         """Init"""
         username = os.environ.get("ROBINHOOD_USERNAME")
         password = os.environ.get("ROBINHOOD_PASSWORD")
-        r.login(username, password)
+        mfa_totp_key = os.environ.get("MFA_TOTP_KEY")
+        totp  = pyotp.TOTP(mfa_totp_key).now()
+        r.login(username, password, mfa_code=totp)
 
     def get_stock_data(self):
         """Get stock data"""
         return r.build_holdings()
+
+    def get_crypto_data(self):
+        """Get crypto data"""
+        return r.crypto.get_crypto_positions()
